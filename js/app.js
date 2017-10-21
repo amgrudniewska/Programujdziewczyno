6fae24094a55b6dd736231fd6daa56d3e8116f89
amgrudniewska <am.grudniewska@gmail.com>
2017-10-21 16:44:29 +0200

    2gie zadanie pętle/przyciski/zwijanie-pojawianie tekstu

--- js/app.js
+++ js/app.js
@@ -14,19 +14,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var buttons = document.querySelectorAll('.read-more');
 
-  function showHide(){
-            var textArea = this.previousElementSibling;
-            if (textArea.style.display === 'none' || textArea.style.display === '') {
-           textArea.style.display = "block";
-           this.innerHTML = 'mniej<span class="glyphicon glyphicon-chevron-up"></span>';
-        } else {
-          textArea.style.display = "none";
-          this.innerHTML = 'więcej<span class="glyphicon glyphicon-chevron-down"></span>';
-        }
-      }
+  function showHide() {
+    var textArea = this.previousElementSibling;
+    if (textArea.style.display === 'none' || textArea.style.display === '') {
+      textArea.style.display = "block";
+      this.innerHTML = 'mniej<span class="glyphicon glyphicon-chevron-up"></span>';
+    } else {
+      textArea.style.display = "none";
+      this.innerHTML = 'więcej<span class="glyphicon glyphicon-chevron-down"></span>';
+    }
+  }
 
-for(var i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener("click", showHide);
-}
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", showHide);
+  }
+
+  //zadanie 3//
+
+  var Navigation = document.querySelector("nav.navbar");
+
+  window.addEventListener("scroll", function() {
+    Navigation.style.borderBottom = "1px solid #7f7f7f";
 
   });
+});
