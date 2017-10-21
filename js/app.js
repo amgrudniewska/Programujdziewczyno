321311e9369c6b4aaf03045bbc3bafb7a63baaee
amgrudniewska <am.grudniewska@gmail.com>
2017-10-21 16:26:49 +0200

    2gie zadanie pętle/przyciski/zwijanie-pojawianie tekstu

--- js/app.js
+++ js/app.js
@@ -1,15 +1,32 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function() {
 
-var menuElement = document.querySelector(".for-dropdown");
-var menuList = document.querySelector(".for-dropdown .dropdown");
+  var menuElement = document.querySelector(".for-dropdown");
+  var menuList = document.querySelector(".for-dropdown .dropdown");
 
-menuElement.addEventListener("mouseover", function() {
-  menuList.style.display = "block";
-menuElement.addEventListener("mouseout", function() {
-  menuList.style.display ="none";
-});
+  menuElement.addEventListener("mouseover", function() {
+    menuList.style.display = "block";
+  });
+  menuElement.addEventListener("mouseout", function() {
+    menuList.style.display = "none";
+  });
+  // zadanie 2
+
+  var buttons = document.querySelectorAll('.read-more');
+
+  function showHide(){
+            var textArea = this.previousElementSibling;
+            if (textArea.style.display === 'none' || textArea.style.display === '') {
+           textArea.style.display = "block";
+           this.innerHTML = 'mniej<span class="glyphicon glyphicon-chevron-up"></span>';
+        } else {
+          textArea.style.display = "none";
+          this.innerHTML = 'więcej<span class="glyphicon glyphicon-chevron-down"></span>';
+        }
+      }
+
+for(var i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", showHide);
+}
 
   });
-
-})
